e77c378013fcdcb9f02d88f31b679884c201c80a
Rafsan12 <rafsan.web.2022@gmail.com>
2025-01-21 22:35:04 +0600

    filter cart data user email wise

--- index.js
+++ index.js
@@ -37,6 +37,12 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await catsCollection.find(query).toArray();
+      res.send(result);
+    });
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await catsCollection.insertOne(cartItem);
